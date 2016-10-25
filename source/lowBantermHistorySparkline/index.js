01f1d296fc558d7b2dedf082b8a4e5ee1da5c774
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-25 15:55:35 -0700

    WIP

--- source/lowBantermHistorySparkline/index.js
+++ source/lowBantermHistorySparkline/index.js
@@ -1,12 +1,12 @@
 import {readFileSync} from "fs"
 import {join} from "path"
-import bantermHistoryCalculation from "../bantermHistoryCalculation"
+import historyBantermCalculation from "../historyBantermCalculation"
 
 export const PATH = [__dirname, "..", "..", "data", "banterms", "high.json"]
 export const LIST = JSON.parse(readFileSync(join(...PATH)))
 export const VALUE = 1
 
-const model = bantermHistoryCalculation([VALUE, LIST])
+const model = historyBantermCalculation([VALUE, LIST])
 
 // : Message: Map with keys body
 // : History: Array of Message
--- source/profileKeywordScore/index.js
+++ source/profileKeywordScore/index.js
@@ -1,4 +1,3 @@
-
 export default function profileKeywordScore () {
   return 0
 }
